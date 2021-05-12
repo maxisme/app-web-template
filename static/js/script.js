421e53c044f6031f3fe35656858f9f157df83fac
Maximilian Mitchell <max@max.me.uk>
2021-05-12 14:15:50 +0100

    only show download button for macos devices

--- static/js/script.js
+++ static/js/script.js
@@ -1,56 +1,70 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top
-	}, 500);
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
 
 $(document).ready(function () {
-	/* only show download for macos */
-	if(navigator.platform.indexOf('Mac') > -1){
-		$("#download").show();
+	function iOS() {
+		return [
+				'iPad Simulator',
+				'iPhone Simulator',
+				'iPod Simulator',
+				'iPad',
+				'iPhone',
+				'iPod'
+			].includes(navigator.platform)
+			// iPad on iOS 13 detection
+			|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 	}
+    var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 
-	/* convert svgs */
-	jQuery('img').filter(function() {
-		return this.src.match(/.*\.svg$/);
-	}).each(function(){
-		var $img = jQuery(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
+    /* only show download for macos */
+    if (!iOS() && isMac) {
+        $("#download").show();
+    }
 
-		jQuery.get(imgURL, function(data) {
-			// Get the SVG tag, ignore the rest
-			var $svg = jQuery(data).find('svg');
+    /* convert svgs */
+    jQuery('img').filter(function () {
+        return this.src.match(/.*\.svg$/);
+    }).each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
 
-			// Add replaced image's ID to the new SVG
-			if(typeof imgID !== 'undefined') {
-				$svg = $svg.attr('id', imgID);
-			}
-			// Add replaced image's classes to the new SVG
-			if(typeof imgClass !== 'undefined') {
-				$svg = $svg.attr('class', imgClass+' replaced-svg');
-			}
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
 
-			// Remove any invalid XML tags as per http://validator.w3.org
-			$svg = $svg.removeAttr('xmlns:a');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
 
-			// Replace image with new SVG
-			$img.replaceWith($svg);
-			$img.css("visibility","1");
-		}, 'xml');
-	});
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+            $img.css("visibility", "1");
+        }, 'xml');
+    });
 });
 
-function slideIn(el){
-	$(el).css("visibility","");
-	$(el).css("position","relative");
-	$(el).css("opacity","0");
-	$(el).css("left","-=200");
-	$(el).animate({
-		"opacity": 1,
-		"left": "+=200"
-	}, 1000);
+function slideIn(el) {
+    $(el).css("visibility", "");
+    $(el).css("position", "relative");
+    $(el).css("opacity", "0");
+    $(el).css("left", "-=200");
+    $(el).animate({
+        "opacity": 1,
+        "left": "+=200"
+    }, 1000);
 }
